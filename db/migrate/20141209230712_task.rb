2c389e92be19f4ae42210607f07a86e88007ff96
Taylor Williams <taylorwilliams1@gmail.com>
2014-12-09 18:11:34 -0500

    table migration

---
+++ db/migrate/20141209230712_task.rb
@@ -0,0 +1,12 @@
+class Task < ActiveRecord::Migration
+  def change
+  	create_table :tasks do |t|
+  		t.string :name
+  		t.text :notes
+  		t.datetime :due_date
+  		t.boolean :completed
+
+  		t.timestamps
+  end
+end
+
